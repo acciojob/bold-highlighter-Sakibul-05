22a779792b4aeb27bfdc79bdc71add98c9396814
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-06-18 10:27:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+let boldText = document.getElementsByTagName("strong");
 function highlight() {
     //Write your code here
-
+	boldText.style.color = "green";
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	boldText.style.color = "#ffff";
     
 }
